b69344bbf93cd0b034a454e3707ef0613230f789
Joel <58995538+jtpotato@users.noreply.github.com>
2024-01-11 23:05:02 +1100

    Initial commit

---
+++ workspaces/example/project.ts
@@ -0,0 +1,3 @@
+import { scene1 } from "./scene1";
+
+scene1()
---
+++ workspaces/example/scene1.ts
@@ -0,0 +1,10 @@
+import { Scene } from "concise-motion-core"
+
+export function scene1() {
+  let scene = new Scene()
+
+  scene.circle()
+  
+  scene.finish()
+}
+
