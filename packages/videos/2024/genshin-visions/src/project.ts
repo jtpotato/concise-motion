7669cbf813955330a19785e30059212ae416017d
Joel <58995538+jtpotato@users.noreply.github.com>
2024-01-13 17:58:11 +1100

    30 seconds in!

--- packages/videos/2024/genshin-visions/src/project.ts
+++ packages/videos/2024/genshin-visions/src/project.ts
@@ -3,7 +3,9 @@ import {makeProject} from '@motion-canvas/core';
 import pythagIntro from './scenes/pythag_intro?scene';
 import comparePersonalities from './scenes/compare_personalities?scene';
 import visionIntro from './scenes/vision_intro?scene';
+import euclidianDistance from './scenes/euclidian_distance?scene';
+import makeWhatever from './scenes/make_whatever?scene';
 
 export default makeProject({
-  scenes: [visionIntro, comparePersonalities, pythagIntro],
+  scenes: [visionIntro, comparePersonalities, pythagIntro, euclidianDistance, makeWhatever],
 });
---
+++ packages/videos/2024/genshin-visions/src/scenes/euclidian_distance.tsx
@@ -0,0 +1,53 @@
+import { makeScene2D } from "@motion-canvas/2d";
+import { CodeBlock, edit } from "@motion-canvas/2d/lib/components/CodeBlock";
+import { createRef, waitFor } from "@motion-canvas/core";
+
+// euclidian distance function
+// function euclidianDistance(array1: number[], array2: number[]) {
+//   if (array1.length != array2.length) throw "Arrays are not the same length.";
+
+//   const differences = [];
+//   for (let i = 0; i < array1.length; i++) {
+//     differences.push((array2[i] - array1[i]) ** 2);
+//   }
+
+//   return Math.sqrt(differences.reduce((partialSum, a) => partialSum + a, 0));
+// }
+
+export default makeScene2D(function* (view) {
+  view.fill('black')
+  const codeRef = createRef<CodeBlock>()
+
+  yield view.add(<>
+    <CodeBlock ref={codeRef} code={`
+      const differences = [];
+      for (let i = 0; i < array1.length; i++) {
+        differences.push((array2[i] - array1[i]) ** 2);
+      }
+    
+      return Math.sqrt(differences.reduce((partialSum, a) => partialSum + a, 0));
+    `} language="ts" scale={0.8} />
+  </>)
+
+  yield* codeRef().edit(4)`
+  ${edit('', 'function euclidianDistance(array1: number[], array2: number[]) {')}
+    const differences = [];
+    for (let i = 0; i < array1.length; i++) {
+      differences.push((array2[i] - array1[i]) ** 2);
+    }
+
+    return Math.sqrt(differences.reduce((partialSum, a) => partialSum + a, 0));
+  ${edit('', '}')}
+  `
+
+  yield* codeRef().edit(1)`
+  ${edit(`  function euclidianDistance(array1: number[], array2: number[]) {
+    const differences = [];
+    for (let i = 0; i < array1.length; i++) {
+      differences.push((array2[i] - array1[i]) ** 2);
+    }
+
+    return Math.sqrt(differences.reduce((partialSum, a) => partialSum + a, 0));
+  }`, '')}
+  `
+})
