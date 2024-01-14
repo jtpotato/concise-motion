5ad8e5809f0cff9369b200f432fad3f0e43e6be7
Joel <58995538+jtpotato@users.noreply.github.com>
2024-01-14 23:00:47 +1100

    begin adding documentation

--- packages/concise-motion-core/src/index.ts
+++ packages/concise-motion-core/src/index.ts
@@ -7,10 +7,17 @@ import { shapeDefaults } from "./shapeDefaults";
 import { fadeToPos } from "./fadeToPos";
 import { triangle } from "./triangle";
 
+/**
+ * Traces a Curve from start to finish over 2 seconds.
+ */
 export function trace(line: Reference<Curve>) {
   return line().end(1, 2);
 }
 
+/**
+ * Returns the default values for LaTeX.
+ * @returns ```{ scale: 4, opacity: 0 }```
+ */
 export function latexDefaults() {
   return {
     scale: 4,
@@ -18,6 +25,9 @@ export function latexDefaults() {
   };
 }
 
+/**
+ * Fades a given Shape or Curve out, moving it to the origin simultaneously. Happens over `1` second.
+ */
 export function fadeOut(shape: Reference<Shape> | Reference<Curve>) {
   return all(shape().opacity(0, 1), shape().x(0, 1), shape().y(0, 1));
 }
