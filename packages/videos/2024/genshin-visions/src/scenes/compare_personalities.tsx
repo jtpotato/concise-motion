d33e2601cee7712a2694158bfd6c9fb2d6074243
Joel <58995538+jtpotato@users.noreply.github.com>
2024-01-18 08:13:09 +1100

    clean code

--- packages/videos/2024/genshin-visions/src/scenes/compare_personalities.tsx
+++ packages/videos/2024/genshin-visions/src/scenes/compare_personalities.tsx
@@ -1,6 +1,6 @@
-import { Circle, Line, makeScene2D, Rect, Txt } from "@motion-canvas/2d";
-import { PossibleVector2, SignalValue, Vector2, all, createRef, createSignal, delay, waitUntil } from "@motion-canvas/core";
-import { cascade, fadeInFrom, fadeOut, fadeOutLine, fadeToPos, shapeDefaults } from "concise-motion-core";
+import { Circle, makeScene2D, Rect, Txt } from "@motion-canvas/2d";
+import { createRef, waitFor } from "@motion-canvas/core";
+import { cascade, fadeInFrom, fadeOut, shapeDefaults } from "concise-motion-core";
 
 export default makeScene2D(function* (view) {
   view.fill('black')
@@ -8,12 +8,29 @@ export default makeScene2D(function* (view) {
   const viewRef = createRef<Rect>()
 
   const person = createRef<Circle>()
+  const labelRef = createRef<Txt>()
 
   view.add(<>
     <Rect ref={viewRef}>
-      <Circle {...shapeDefaults()} ref={person} />
+      <Circle {...shapeDefaults()} ref={person}>
+
+      </Circle>
+      <Txt ref={labelRef} text="???" fill="white" fontSize={60} x={0} y={0} fontFamily={"monospace"} opacity={0} />
     </Rect>
   </>)
 
-  yield* fadeInFrom("bottom", person, [0, -100])
+  yield* cascade(0.2,
+    fadeInFrom("bottom", person, [0, -100]),
+    fadeInFrom("bottom", labelRef, [0, 100])
+  )
+
+  labelRef().text("Pyro?")
+  yield* person().fill('#bc0000', 0.5)
+  yield* waitFor(0.5)
+  labelRef().text("Cryo?")
+  yield* person().fill('#b9dfe4', 0.5)
+  yield* cascade(0.2,
+    fadeOut(person),
+    fadeOut(labelRef)
+  )
 })
